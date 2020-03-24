59117c0c9bcbede10e3a4c5e38cea6b6d2449935
Keyxllai <jxlxl2010@126.com>
2020-03-24 10:39:13 +0800

    monitor

--- src/NodeStart.ts
+++ src/NodeStart.ts
@@ -11,17 +11,22 @@ var workstation = new WorkStation();
 
 workstation.start();
 
+let monitor = function () {
+    const memoryUsage = process.memoryUsage();
+    const cpuUsage = process.cpuUsage();
+    console.log("Memory Usage: " + JSON.stringify({
+        rss: util.byteToMB(memoryUsage.rss),                // 进程占用内存，包括代码本身、栈、堆
+        heapTotal: util.byteToMB(memoryUsage.heapTotal),    // 堆申请到的内存
+        heapUsed: util.byteToMB(memoryUsage.heapUsed),      // 堆使用的内存，判断内存泄漏主要依据字段
+        external: util.byteToMB(memoryUsage.external),
+    }));
 
-const memoryUsage = process.memoryUsage();
-const cpuUsage = process.cpuUsage();
-console.log("Memory Usage: " + JSON.stringify({
-    rss: util.byteToMB(memoryUsage.rss),                // 进程占用内存，包括代码本身、栈、堆
-    heapTotal: util.byteToMB(memoryUsage.heapTotal),    // 堆申请到的内存
-    heapUsed: util.byteToMB(memoryUsage.heapUsed),      // 堆使用的内存，判断内存泄漏主要依据字段
-    external: util.byteToMB(memoryUsage.external),
-}));
+    console.log("CPU Usage: " + JSON.stringify({
+        user: util.byteToMB(cpuUsage.user),
+        system: util.byteToMB(cpuUsage.system)
+    }));
+}
 
-console.log("CPU Usage: " + JSON.stringify({
-    user: util.byteToMB(cpuUsage.user),
-    system: util.byteToMB(cpuUsage.system)
-}));
+setInterval(function(){
+    monitor();
+},10000);
